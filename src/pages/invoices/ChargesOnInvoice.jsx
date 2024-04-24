d1dacb61a718434d847b15dfa0b9b33c62bcccb1
guitarbuju <gerardoamadrid@gmail.com>
2024-04-24 17:18:45 +0200

    fix total in form

--- src/pages/invoices/ChargesOnInvoice.jsx
+++ src/pages/invoices/ChargesOnInvoice.jsx
@@ -1,5 +1,4 @@
-
-import {  getDniData } from "../../api/fetchData";
+import { getDniData } from "../../api/fetchData";
 import { useEffect, useState } from "react";
 // import play from "../../assets/icons8-play-50.png";
 import { useAppointmentsInfoStore } from "../../../store";
@@ -9,18 +8,18 @@ const ChargesOnInvoice = () => {
   const [chargeList, setChargeList] = useState([]);
   const BASE_URL = import.meta.env.VITE_BASE_URL;
   const url = `${BASE_URL}/charges`;
-  const admissionsInfo= useAppointmentsInfoStore(
+  const admissionsInfo = useAppointmentsInfoStore(
     (state) => state.appointmentInfo
   );
-//   const setAdmissionsInfo = useAppointmentsInfoStore(
-//     (state) => state.setAppointmentInfo
-//   );
-//   const navigate=useNavigate()
+  //   const setAdmissionsInfo = useAppointmentsInfoStore(
+  //     (state) => state.setAppointmentInfo
+  //   );
+  //   const navigate=useNavigate()
 
   useEffect(() => {
     const getCharges = async () => {
       try {
-        const chargeList = await getDniData(url,admissionsInfo.id);
+        const chargeList = await getDniData(url, admissionsInfo.id);
         setChargeList(chargeList);
       } catch (error) {
         console.error(error);
@@ -30,20 +29,15 @@ const ChargesOnInvoice = () => {
     getCharges();
   }, []);
 
- 
-  
   console.log(chargeList);
   console.log(admissionsInfo);
-  
-  
+
   return (
-    <div >
-   
+    <div>
       <div className="container p-2 mx-auto sm:p-4 text-gray-900">
         <h2 className="mb-4 text-2xl font-semibold leading-tight ">
-          Outstanding Charges 
+          Outstanding Charges
         </h2>
-       
 
         <div className="overflow-x-auto">
           <table className="w-full p-6 text-xs text-left whitespace-nowrap">
@@ -55,8 +49,6 @@ const ChargesOnInvoice = () => {
                 <th className="p-3  border-x border-y">Quantity</th>
                 <th className="p-3  border-x border-y">Price</th>
                 <th className="p-3  border-x border-y">Total</th>
-                
-                
               </tr>
             </thead>
             <tbody className="border-b border-gray-300">
@@ -101,10 +93,13 @@ const ChargesOnInvoice = () => {
               ))}
             </tbody>
           </table>
-          
         </div>
-        <span className="text-md mt-8">Total Charges:{chargeList?.total[0]?.total_sum}</span>
       </div>
+      {chargeList?.total && chargeList.total.length > 0 && (
+        <span className="text-md mt-2 flex justify-end">
+          Total Charges: {chargeList.total[0].total_sum}
+        </span>
+      )}
     </div>
   );
 };
