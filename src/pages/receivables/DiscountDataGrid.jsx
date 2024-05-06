3c42d62bc9b44b5998e055874e077538d2fbbbdf
guitarbuju <gerardoamadrid@gmail.com>
2024-05-06 22:25:37 +0200

    working fine

--- src/pages/receivables/DiscountDataGrid.jsx
+++ src/pages/receivables/DiscountDataGrid.jsx
@@ -1,6 +1,6 @@
-
 import { useState } from "react";
 import { postPersonData } from "../../api/fetchData";
+import DiscountModal from "./DiscountModal";
 
 const DiscountDataGrid = ({ foundPatient }) => {
   const BASE_URL = import.meta.env.VITE_BASE_URL;
@@ -8,6 +8,8 @@ const DiscountDataGrid = ({ foundPatient }) => {
 
   const [discount, setDiscount] = useState("");
   const [hiddenRows, setHiddenRows] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [clickedAdmissionId, setClickedAdmissionId] = useState(null);
 
   const inputChangeHandler = (e) => {
     setDiscount(e.target.value);
@@ -22,14 +24,15 @@ const DiscountDataGrid = ({ foundPatient }) => {
     console.log(dataToApi);
     const dataPost = await postPersonData(url, dataToApi);
     setHiddenRows([...hiddenRows, admissionId]);
-     console.log(dataPost);
+    console.log(dataPost);
+    setIsOpen(false);
   };
 
   return (
     <div className="bg-zinc-50">
       <div className="container p-2 mx-auto sm:p-4 text-gray-900">
         <h2 className="mb-4 text-2xl font-semibold leading-tight text-gray-900">
-          Found Admissions for Patient
+          Found Admissions for Patients
         </h2>
         <div className="overflow-x-auto">
           {foundPatient?.status !== 200 ? (
@@ -43,14 +46,21 @@ const DiscountDataGrid = ({ foundPatient }) => {
                   <th className="p-2 border-x border-y w-10">Admission Id</th>
                   <th className="p-2 border-x border-y w-10">Invoice Id</th>
                   <th className="p-3 border-x border-y w-8">Total</th>
+                  <th className="p-3 border-x border-y w-8">Balance</th>
                   <th className="p-3 border-x border-y w-8">Discount</th>
                   <th className="p-3 border-x border-y w-8">Submit</th>
                 </tr>
               </thead>
               <tbody className="border-b dark:bg-gray-50 dark:border-gray-300">
                 {foundPatient?.data?.data?.map((admission, admission_id) => (
-                  <tr className={hiddenRows.includes(admission.admission_id) ? "hidden" : "text-sm"} 
-                  key={admission_id}>
+                  <tr
+                    className={
+                      hiddenRows.includes(admission.admission_id)
+                        ? "hidden"
+                        : "text-sm"
+                    }
+                    key={admission_id}
+                  >
                     <td className="px-3 py-2">
                       <p>{admission.patient_full_name}</p>
                     </td>
@@ -66,6 +76,9 @@ const DiscountDataGrid = ({ foundPatient }) => {
                     <td className="px-3 py-2">
                       <p>{admission.invoice_total}</p>
                     </td>
+                    <td className="px-3 py-2">
+                      <p>{admission.net_amount}</p>
+                    </td>
                     <td className="px-3 py-2 hidden">
                       <input
                         className="border rounded-md py-1 w-20"
@@ -73,7 +86,6 @@ const DiscountDataGrid = ({ foundPatient }) => {
                         onChange={(e) =>
                           inputChangeHandler(e, admission.admission_id)
                         }
-                       
                       />
                     </td>
                     <td className="px-3 py-2">
@@ -88,7 +100,10 @@ const DiscountDataGrid = ({ foundPatient }) => {
                       <button
                         type="button"
                         className="h-6 ml-2 mt-1 bg-medBlue hover:bg-cyan-700 text-gray-100 px-2 rounded transition duration-150 text-xs"
-                        onClick={() => clickHandler(admission.admission_id)}
+                        onClick={() => {
+                          setIsOpen(true);
+                          setClickedAdmissionId(admission.admission_id);
+                        }}
                       >
                         Get Discount
                       </button>
@@ -100,6 +115,12 @@ const DiscountDataGrid = ({ foundPatient }) => {
           )}
         </div>
       </div>
+      <DiscountModal
+        clickHandler={clickHandler}
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        admissionId={clickedAdmissionId}
+      />
     </div>
   );
 };
