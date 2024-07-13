627bc8d77c9b6bcf69e6a823354e097e7b08dcf0
guitarbuju <gerardoamadrid@gmail.com>
2024-07-13 21:11:56 +0200

    tabs fix

--- src/pages/doctor's area/components/Tabs.jsx
+++ src/pages/doctor's area/components/Tabs.jsx
@@ -1,7 +1,6 @@
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
 import MedicalData from './MedicalData';
 import Recipe from './Recipe';
-import PdfMedicalChart from '../pdf/PdfMedicalChart';
 import ChartViewer from './ChartViewer';
 
 function Tabs() {
@@ -11,14 +10,14 @@ function Tabs() {
        <Tab className="text-sm rounded  px-4 data-[selected]:bg-yellow-400 data-[selected]:text-white data-[selected]:underline"> Notes</Tab>
         <Tab className="text-sm rounded px-4 data-[selected]:bg-yellow-400 data-[selected]:text-white data-[selected]:underline">Recipe</Tab>
         <Tab className="text-sm rounded px-4 data-[selected]:bg-yellow-400 data-[selected]:text-white data-[selected]:underline">Orders</Tab>
-        <Tab className="text-sm rounded px-4 data-[selected]:bg-yellow-400 data-[selected]:text-white data-[selected]:underline">Pdf</Tab>
+        
        
       </TabList>
       <TabPanels>
         <TabPanel><MedicalData/></TabPanel>
         <TabPanel><Recipe/></TabPanel>
         <TabPanel><ChartViewer/></TabPanel>
-        <TabPanel><PdfMedicalChart/></TabPanel>
+        
       </TabPanels>
     </TabGroup>
   )
